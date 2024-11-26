02c9db210f3ed9d56f702cd93aaa007c6cf0bd26
Jiaqi-Xing <ariel.xing@mail.utoronto.ca>
2024-11-26 02:37:43 -0500

    Upload script of data cleaning process

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,35 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw bridge conditions data
+# Author: Ariel Xing
+# Date: 24 November 2024
+# Contact: ariel.xing@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: raw bridge conditions data
+# Any other information needed? None
 
 #### Workspace setup ####
 library(tidyverse)
+library(stringr)
+library(arrow)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
-  tidyr::drop_na()
+# Read the data 
+raw_data <- read_delim(
+  file = "data/01-raw_data/raw_bridge_conditions_data.txt",
+  delim = "\t",              # Tab delimiter
+  na = c("", "NA"),          # Treat blanks as NA
+  trim_ws = TRUE,            # Trim whitespace
+  guess_max = 20000          # Ensure guessing column types works for all rows
+)
+
+# Modify the Municipality column to keep only the last word
+raw_data$Municipality <- str_extract(raw_data$Municipality, "\\w+$")
+
+# Extract useful columns and remove N/A
+cleaned_data <- raw_data %>%
+  select(County, Municipality, Condition, AgeAtInspection, SD.FO.Status) %>%
+  drop_na()
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_parquet(cleaned_data, "data/02-analysis_data/cleaned_data.parquet")
